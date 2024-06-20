2d4a15ff8937c3f2ae5ee61b8cb1a19f6b792efd
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-06-21 02:51:26 +0530

    update(Order): added feignClient to call inventory service

--- src/main/java/com/rishmi5h/microservices/order/service/OrderService.java
+++ src/main/java/com/rishmi5h/microservices/order/service/OrderService.java
@@ -1,7 +1,7 @@
 package com.rishmi5h.microservices.order.service;
 
 
-import com.rishmi5h.microservices.order.dto.OrderLineItemsDto;
+import com.rishmi5h.microservices.order.client.InventoryClient;
 import com.rishmi5h.microservices.order.dto.OrderRequest;
 import com.rishmi5h.microservices.order.model.Order;
 import com.rishmi5h.microservices.order.repository.OrderRepository;
@@ -17,16 +17,25 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
 
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
+
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException(orderRequest.skuCode() + " is out of stock" );
+        }
     }
+
 
 }
